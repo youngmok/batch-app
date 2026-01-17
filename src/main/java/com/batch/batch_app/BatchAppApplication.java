43edd31f5e9a04ddeddaaae0c06ab8df7a73fa41
unknown <>
2026-01-17 14:45:57 +0900

    Initial Commit

---
+++ src/main/java/com/batch/batch_app/BatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.batch.batch_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/batch_app/BatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.batch_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
